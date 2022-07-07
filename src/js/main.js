28af9ab3e6fb762c3afd480e547ecdd9d0ebed07
Alice <andreevaalisa42@gmail.com>
2022-07-07 13:53:05 +0300

    add forms to the project

---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+import modals from "./modules/modal";
+import sliders from "./modules/sliders";
+
+modals();
+sliders('.feedback-slider-item', '', '.main-prev-btn', '.main-next-btn');
+sliders('.main-slider-item', 'vertical');
---
+++ src/js/modules/sliders.js
@@ -0,0 +1,75 @@
+const sliders = (slides, dir, prev, next) => {
+  let slideIndex = 1;
+  let paused = false;
+  const items = document.querySelectorAll(slides);
+
+  function showSlides(n) {
+    if(n > items.length) {
+      slideIndex = 1;
+    }
+
+    if (n < 1) {
+      slideIndex = items.length;
+    }
+
+    items.forEach(item => {
+      item.classList.add('animated');
+      item.style.display = 'none';
+    });
+
+    items[slideIndex - 1].style.display = 'block';
+  }
+
+  showSlides(slideIndex);
+
+  function changeSlides(n) {
+    showSlides(slideIndex += n);
+  }
+
+  try {
+    const prevBtn = document.querySelector(prev);
+    const nextBtn = document.querySelector(next);
+
+    prevBtn.addEventListener('click', () => {
+      changeSlides(-1);
+      items[slideIndex - 1].classList.remove('slideInLeft');
+      items[slideIndex - 1].classList.add('slideInRight');
+    });
+
+    nextBtn.addEventListener('click', () => {
+      changeSlides(1);
+      items[slideIndex - 1].classList.remove('slideInRight');
+      items[slideIndex - 1].classList.add('slideInLeft');
+    });
+
+  } catch (err) {
+    console.log(err);
+  }
+
+  function activateAnnimation() {
+    if( dir === 'vertical') {
+      paused = setInterval(() => {
+        changeSlides(1);
+        items[slideIndex - 1].classList.add('slideInDown');
+      }, 6000);
+    } else {
+      paused = setInterval(() => {
+        changeSlides(1);
+        items[slideIndex - 1].classList.remove('slideInRight');
+        items[slideIndex - 1].classList.add('slideInLeft');
+      }, 6000);
+    }
+  }
+
+  activateAnnimation();
+
+  items[0].parentNode.addEventListener('mouseenter', () => {
+    clearInterval(paused);
+  });
+
+  items[0].parentNode.addEventListener('mouseleave', () => {
+    activateAnnimation();
+  });
+};
+
+export default sliders;
