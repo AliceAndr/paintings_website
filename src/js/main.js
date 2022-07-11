ab86701c4f278e729b70f076f743ff99da976f43
Alice <andreevaalisa42@gmail.com>
2022-07-11 21:48:53 +0300

    add show-more-examples module

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ import sliders from "./modules/sliders";
 import forms from "./modules/forms";
 import mask from "./modules/mask";
 import checkTextInputs from "./modules/checkTextInputs";
+import showMoreStyles from "./modules/showMoreStyles";
 
 modals();
 sliders('.feedback-slider-item', '', '.main-prev-btn', '.main-next-btn');
@@ -11,3 +12,4 @@ forms();
 mask('[name="phone"]');
 checkTextInputs('[name="name"]');
 checkTextInputs('[name="message"]');
+showMoreStyles('.button-styles', '.styles-2')
