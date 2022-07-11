295e53a3897b4a4700a193837804a35723e5c2f4
Alice <andreevaalisa42@gmail.com>
2022-07-11 18:45:32 +0300

    add mask

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,13 @@
 import modals from "./modules/modal";
 import sliders from "./modules/sliders";
+import forms from "./modules/forms";
+import mask from "./modules/mask";
+import checkTextInputs from "./modules/checkTextInputs";
 
 modals();
 sliders('.feedback-slider-item', '', '.main-prev-btn', '.main-next-btn');
 sliders('.main-slider-item', 'vertical');
+forms();
+mask('[name="phone"]');
+checkTextInputs('[name="name"]');
+checkTextInputs('[name="message"]');
